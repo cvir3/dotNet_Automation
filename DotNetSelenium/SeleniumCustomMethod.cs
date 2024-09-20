5f0794158d5d7682458f1adbf39c557950a9e0f1
Viren Chauhan <vir3.chauhan@gmail.com>
2024-09-20 11:41:24 +0530

    Custom Methods
    Create Custom Methods and use in the UnitTest1 class.

---
+++ DotNetSelenium/SeleniumCustomMethod.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.Events;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetSelenium
+{
+    public class SeleniumCustomMethod
+    {
+        public static void Click(IWebDriver driver, By locator)
+        {
+            driver.FindElement(locator).Click();
+        }
+
+        public static void EnterText(IWebDriver driver, By locator, string text)
+        {
+            driver.FindElement(locator).Clear();
+            driver.FindElement(locator).SendKeys(text);
+        }
+
+        public static void SelectDropDownByText(IWebDriver driver, By locator, string value)
+        {
+            SelectElement selectElement = new SelectElement(driver.FindElement(locator));
+            selectElement.SelectByText(value);
+        }
+
+        public static void MultiSelectElements(IWebDriver driver, By locator, string[] values)
+        {
+            SelectElement multiSelect = new SelectElement(driver.FindElement(locator));
+            foreach (var value in values)
+            {
+                multiSelect.SelectByValue(value);
+            }
+        }
+
+        public static List<string> GetAllSelectedLists(IWebDriver driver, By locator)
+        {
+            List<string> options = new List<string>();
+            SelectElement multiSelect = new SelectElement(driver.FindElement(locator));
+            IList<IWebElement> selectedOption = multiSelect.AllSelectedOptions;
+            foreach (IWebElement option in selectedOption)
+            {
+                options.Add(option.Text);
+            }
+            return options;
+        }
+        
+    }
+}
--- DotNetSelenium/UnitTest1.cs
+++ DotNetSelenium/UnitTest1.cs
@@ -30,14 +30,10 @@ namespace DotNetSelenium
             IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_custrec_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
             driver.Manage().Window.Maximize();
-            IWebElement signInLink = driver.FindElement(By.LinkText("Create your Amazon account"));
-            signInLink.Click();
-            IWebElement txtUserName = driver.FindElement(By.Name("customerName"));
-            txtUserName.SendKeys("HelloUser");
-            IWebElement txtMobile = driver.FindElement(By.Name("email"));
-            txtMobile.SendKeys("9898098985");
-            IWebElement txtPassword = driver.FindElement(By.Name("password"));
-            txtPassword.SendKeys("HelloPassword");
+            SeleniumCustomMethod.Click(driver, By.LinkText("Create your Amazon account"));
+            SeleniumCustomMethod.EnterText(driver, By.Name("customerName"), "HelloUser");
+            SeleniumCustomMethod.EnterText(driver, By.Name("email"), "9898098985");
+            SeleniumCustomMethod.EnterText(driver, By.Name("password"), "HelloPassword");
             IWebElement btnSubmit = driver.FindElement(By.Id("continue"));
             btnSubmit.Click();
             Thread.Sleep(1000);
@@ -50,18 +46,12 @@ namespace DotNetSelenium
             driver.Navigate().GoToUrl("https://www.letskodeit.com/practice");
             driver.Manage().Window.Maximize();
             Thread.Sleep(1000);
-            SelectElement selectElement = new SelectElement(driver.FindElement(By.Id("carselect")));
-            selectElement.SelectByText("Benz");
+            SeleniumCustomMethod.SelectDropDownByText(driver, By.Id("carselect"), "Benz");
             Thread.Sleep(1000);
-            SelectElement multiSelect = new SelectElement(driver.FindElement(By.Id("multiple-select-example")));
-            multiSelect.SelectByText("Orange");
-            multiSelect.SelectByText("Peach");
+            SeleniumCustomMethod.MultiSelectElements(driver, By.Id("multiple-select-example"), ["peach", "orange"]);
             Thread.Sleep(1000);
-            IList<IWebElement> selectedOption = multiSelect.AllSelectedOptions;
-            foreach (IWebElement option in selectedOption)
-            {
-                Console.WriteLine(option.Text);
-            }             
+            var getSelectedOptions = SeleniumCustomMethod.GetAllSelectedLists(driver, By.Id("multiple-select-example"));
+            getSelectedOptions.ForEach(Console.WriteLine);
             driver.Quit();
         }
     }
